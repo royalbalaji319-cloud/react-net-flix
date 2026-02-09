3223a294dbd26e599c02f10441611af87832767c
royalbalaji319-cloud <royalbalaji319@gmail.com>
2026-02-09 15:47:59 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,23 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './App.css'
-import './cssfiles/Navbar.css';
-import './cssfiles/Hero.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+
+// Global CSS
+import "./App.css";
+import "./cssfiles/Navbar.css";
+import "./cssfiles/Hero.css";
 import "./cssfiles/SignIn.css";
-import './cssfiles/Trending.css';
-import './cssfiles/FAQ.css';
-import './cssfiles/Footer.css';
+import "./cssfiles/Trending.css";
+import "./cssfiles/FAQ.css";
+import "./cssfiles/Footer.css";
 
-import './i18n';
+// i18n
+import "./i18n";
 
-// import './ReasonsToJoin.css';
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-
-
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
